80565817e11036573ac792f523d9834bce6c1abf
Igor Santos <falaigors@gmail.com>
2021-03-17 08:48:46 -0300

    Estilizaçao do Header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,7 +1,15 @@
+import logoImg from '../../assets/logo.svg'
+import { Container, Content } from './styles'
+
 export function Header() {
   return (
-    <div>
-      Header
-    </div>
+    <Container>
+      <Content>
+        <img src={logoImg} alt="dtmoney" />
+        <button>
+          Nova Transação
+        </button>
+      </Content>
+    </Container>
   )
 }
